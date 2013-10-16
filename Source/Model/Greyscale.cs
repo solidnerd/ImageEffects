845a32a15e9d827f32c425ec7504b9f85c070688
Niclas Mietz <mietz.niclas@gmail.com>
2013-10-16 14:09:56 +0200

    Added PersistentImage + Greyscale and save function implemented

---
+++ Source/Model/Greyscale.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Drawing;
+using System.Collections.Generic;
+
+namespace ImageEffects.Model
+{
+    class Greyscale
+    {
+        private Bitmap targetimage;
+     
+        public Greyscale(Bitmap image) 
+        {
+            targetimage = image;
+      
+        }
+
+
+        public Bitmap startgreyscale() 
+        {
+            for(int x = 0; x < targetimage.Width;x++)
+            {
+                for(int y = 0 ; y < targetimage.Height;y++)
+                {
+                   targetimage.SetPixel(x,y,calculateluminanz(targetimage.GetPixel(x, y)));
+                }
+            }
+
+            return targetimage;
+        }
+ 
+
+        private Color calculateluminanz(Color color) 
+        {
+            return Color.FromArgb((int)(0.299 * color.R + 0.587 * color.G + 0.114 * color.B));
+        }
+
+    }
+}
--- Source/View/Display.cs
+++ Source/View/Display.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using ImageEffects.Controller;
+using ImageEffects.Model;
+using System.Drawing;
+using System.IO;
+using System.Drawing.Imaging;
 
 namespace ImageEffects.View
 {
@@ -9,7 +13,7 @@ namespace ImageEffects.View
         private const string programname = "ImageEffects";
         private string filepath;
         private List<string> menupoints = new List<string>();
-        
+        private Bitmap selectedimage;
         public string Filepath
         {
             get { return filepath; }
@@ -30,15 +34,19 @@ namespace ImageEffects.View
 
        public void show()
        {
-           ConsoleColorChanger.Write(ConsoleColor.Gray, programname + "\t\t");
-           ConsoleColorChanger.WriteLine((filepath != null ? ConsoleColor.Green : ConsoleColor.Red), (filepath != null ? filepath : "No File Loaded"));
-           ConsoleColorChanger.WriteLine(ConsoleColor.Gray, "===============");
+           header();
            for (int i = 0; i < menupoints.Count; i++)
            {
                ConsoleColorChanger.WriteLine(ConsoleColor.Gray, String.Format("({0}) {1}", i + 1, menupoints[i]));
            }
        }
 
+       private void header() 
+       {
+           ConsoleColorChanger.Write(ConsoleColor.Gray, programname + "\t\t");
+           ConsoleColorChanger.WriteLine((filepath != null ? ConsoleColor.Green : ConsoleColor.Red), (filepath != null ? filepath : "No File Loaded"));
+           ConsoleColorChanger.WriteLine(ConsoleColor.Gray, "===============");
+       }
        public void ReadMenuInput() 
        {
            int input = 0;
@@ -53,9 +61,37 @@ namespace ImageEffects.View
            switch (input - 1) 
            {
                case 0:
-                   Console.Clear();
+                   loadimage();
                    break;
+               case 1:
+                   break;
+               case 2:
+                   greyscaleimage();
+                   break;
+
            }
        }
+
+        private void greyscaleimage()
+        {
+           Greyscale greyscaledimage = new Greyscale(selectedimage);
+           Bitmap bitmapsave = greyscaledimage.startgreyscale();
+           bitmapsave.Save(new FileStream("a.png", FileMode.Create), ImageFormat.Png);
+        }
+        private void  loadimage()
+        {
+            Console.Clear();
+            header();
+            ConsoleColorChanger.WriteLine(ConsoleColor.Gray, "Bitte geben Sie den Pfad an:");
+            string imagepath = Console.ReadLine();
+
+            PersistentImage imagefromhdd = new PersistentImage(imagepath);
+            Console.Clear();
+            filepath = imagefromhdd.Path;
+            selectedimage = imagefromhdd.Bitmap;
+            show();
+            Console.Read();
+            
+        }
     }
 }
